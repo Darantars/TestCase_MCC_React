d7c59e121bd4d0783875365fefa8e8c1ece46557
Darantar <Darantar@DESKTOP-EK9VSKR>
2024-06-16 19:43:27 +0700

    Get template

---
+++ mcc_react_testcase/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+          <h1>Hello, kiddo</h1>
+          <button>Продолжить работать</button>
+    </>
+  )
+}
+
+export default App
